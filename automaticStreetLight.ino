a25991ec88f49132f91cc1c33b834af58cfb23e8
thefoodiedad <47085717+thefoodiedad@users.noreply.github.com>
2019-03-01 18:37:10 +0530

    Add files via upload

---
+++ automaticStreetLight.ino
@@ -0,0 +1,31 @@
+// Automatic street light
+
+int Relay1 = 8;
+int Relay2 = 9;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0, INPUT);
+pinMode(Relay1, OUTPUT);
+pinMode(Relay2, OUTPUT);
+
+digitalWrite(Relay1, HIGH);
+digitalWrite(Relay2, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int x = digitalRead(A0);
+if(x<=512)
+{
+  digitalWrite(Relay1, LOW); // lights turned on
+  digitalWrite(Relay2, LOW); // lights turned on
+  delay(2000);
+  
+}
+else
+{
+  digitalWrite(Relay1, HIGH); // lights turned off
+  digitalWrite(Relay1, HIGH); // lights turned off
+  delay(2000);
+}
+}
